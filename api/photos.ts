b7f9655bb552602766a3d911a935fa8a41f32510
Fedor Kurilov <fkuril@yandex.ru>
2020-04-22 17:02:46 +0200

    feat: photos api

---
+++ api/photos.ts
@@ -0,0 +1,28 @@
+import { NowRequest, NowResponse } from '@now/node';
+import https from 'https';
+import querystring from 'querystring';
+
+export default (req: NowRequest, res: NowResponse) => {
+  const UNSPLASH_CLIENT_ID = process.env.UNSPLASH_CLIENT_ID;
+
+  if (!UNSPLASH_CLIENT_ID) {
+    return res.json({
+      error: 'no client id',
+    });
+  }
+
+  const unsplashUrl = 'https://api.unsplash.com/photos?' + querystring.stringify(req.query);
+
+  https.get(
+    unsplashUrl,
+    {
+      headers: {
+        Authorization: `Client-ID ${UNSPLASH_CLIENT_ID}`,
+        'Accept-Version': 'v1',
+      },
+    },
+    unsplashRes => {
+      unsplashRes.pipe(res);
+    },
+  );
+};
